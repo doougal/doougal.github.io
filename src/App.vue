7da2626ca9b756d2bae851cf5c72301f977c5bdf
Alex Burling <d0ougal@yahoo.co.nz>
2021-10-17 15:02:30 +1300

    added more links

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <template>
   <div>
-    
     <header>
       <img id="profilePhoto" alt="Profile Photo" src="./assets/profile.png" />
 
       <h1>Alex Burling</h1>
-      <p>Software Developer</p>
+      <p>System Developer @ Gaia Healthcare NZ</p>
+      <p>BSC Computer Science @ University of Canterbury</p>
     </header>
 
     <div id="links">
@@ -14,25 +14,58 @@
         href="https://github.com/doougal"
         target="_blank"
         rel="noopener"
-        >GitHub</a
+        ><Icon icon="akar-icons:github-fill" /> GitHub</a
       >
       <a
         class="link"
         href="https://www.linkedin.com/in/alex-burling/"
         target="_blank"
         rel="noopener"
-        >LinkedIn</a
+        ><Icon icon="akar-icons:linkedin-fill" /> LinkedIn</a
+      >
+      <a
+        class="link"
+        href="https://app.hackthebox.eu/users/779571"
+        target="_blank"
+        rel="noopener"
+        ><Icon icon="simple-icons:hackthebox" /> HackTheBox</a
+      >
+      <a
+        class="link"
+        href="https://tryhackme.com/p/doog"
+        target="_blank"
+        rel="noopener"
+        ><Icon icon="simple-icons:tryhackme" /> TryHackMe</a
+      >
+      <a
+        class="link"
+        href="https://www.hackerrank.com/burling_alex"
+        target="_blank"
+        rel="noopener"
+        ><Icon icon="fa-brands:hackerrank" /> HackerRank</a
       >
     </div>
 
-    <footer>I made this website myself</footer>
-
+    <footer>
+      <a
+        id="footerLink"
+        href="https://github.com/doougal/doougal.github.io"
+        target="_blank"
+        rel="noopener"
+        >See the code for this site</a
+      >
+    </footer>
   </div>
 </template>
 
 <script>
+import { Icon } from "@iconify/vue";
+
 export default {
   name: "App",
+  components: {
+    Icon,
+  },
 };
 </script>
 
@@ -90,6 +123,17 @@ body {
   font-weight: 500;
   border-radius: 5px;
   border: dashed var(--color) 2px;
+  text-align: center;
+  text-decoration: none;
+  color: var(--color);
+}
+
+#footerLink {
+  display: block;
+  margin-bottom: 20px;
+  padding: 17px;
+  font-size: 1rem;
+  font-weight: 500;
   text-align: center;
   text-decoration: none;
   color: var(--color);
